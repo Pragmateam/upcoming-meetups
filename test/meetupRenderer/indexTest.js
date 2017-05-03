7d4d02185dc206a7343d6c3f295d1057478a2dd0
Alabê Duarte <alabeduarte@gmail.com>
2017-05-03 10:18:58 +1000

    Refactoring test in favor of having a readable timezone representation

--- test/meetupRenderer/indexTest.js
+++ test/meetupRenderer/indexTest.js
@@ -7,7 +7,7 @@ describe('.render', () => {
   it('renders the upcoming meetup', () => {
     const meetup = {
       name: faker.hacker.adjective(),
-      time: 1493884800000,
+      time: new Date('2017-05-04T18:00:00+10:00'),
       duration: 10800000,
       group: {
         name: faker.hacker.noun(),
@@ -15,19 +15,19 @@ describe('.render', () => {
       link: faker.internet.url(),
     };
 
-    const groupName = `*${meetup.group.name}*`;
+    const groupName = `${meetup.group.name}`;
     const meetupName = meetup.name;
     const date = 'May 4, 6:00 PM to 9:00 PM';
     const meetupLink = meetup.link;
 
     expect(MeetupRenderer.render(meetup)).to
-      .eql(`${groupName} :: ${meetupName} - ${date} - ${meetupLink}`);
+      .eql(`*${groupName}* :: ${meetupName} - ${date} - ${meetupLink}`);
   });
 
   it('calculates times for different timezones', () => {
     const meetup = {
       name: faker.hacker.adjective(),
-      time: 1493884800000,
+      time: new Date('2017-05-04T18:00:00+10:00'),
       duration: 10800000,
       venue: {
         city: 'Tokyo',
